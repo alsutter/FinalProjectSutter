b5f5fe98e465ea5714b17a6e476d2364d6b67d71
Annika Sutter <alsutter@dmacc.edu>
2018-04-26 10:10:09 -0500

    Update 4/26/18 at 10:09am
    A few changes, the biggest being moving most of the code from LibraryTesterSutter to the Library class. Then there are a few changes and some comments for my use to figure out my problems.

--- Final Project/src/Library.java
+++ Final Project/src/Library.java
@@ -38,7 +38,7 @@ import java.awt.Font;
  * Then if needed, more instructions will appear and then execute necessary methods to complete request.
  * @author Annika Sutter
  */
-public class Library extends LibraryTesterSutter{ //is the extends necessary??
+public class Library {
 
 	private JFrame frame;
 	private JLabel label;
@@ -48,7 +48,8 @@ public class Library extends LibraryTesterSutter{ //is the extends necessary??
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args){
+	public static void main(String[] args) {
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -72,6 +73,81 @@ public class Library extends LibraryTesterSutter{ //is the extends necessary??
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		//first, create all books and enter information
+		Book library[]=new Book[30]; //new array of books
+		LinkList dueDateList = new LinkList(); //make new list
+		int nElems= 0; //number of items
+		int j; //loop counter
+		
+		//create new book for every spot in the array
+		for (j=0; j<library.length; j++){
+			library[j]=new Book();
+		}
+		
+		//set all info for all the books
+		//book 1
+		library[0].setInfo("The Hunger Games", "Suzanne Collins", "Science Fiction", true);
+		//book 2
+		library[1].setInfo("Catching Fire", "Suzanne Collins", "Science Fiction", true);
+		//book 3
+		library[2].setInfo("Mockingjay", "Suzanne Collins", "Science Fiction", true);
+		//book 4
+		library[3].setInfo("Hush, Hush", "Becca Fitzpatrick", "Romance", true);
+		//book 5
+		library[4].setInfo("Crecendo", "Becca Fitzpatrick", "Romance", true);
+		//book 6
+		library[5].setInfo("Silence", "Becca Fitzpatrick", "Romance", true);
+		//book 7
+		library[6].setInfo("Finale", "Becca Fitzpatrick", "Romance", true);
+		//book 8
+		library[7].setInfo("The Fault In Our Stars", "John Green", "Fiction", true);
+		//book 9
+		library[8].setInfo("Les Miserables", "Victor Hugo", "Histroical Fiction", true);
+		//book 10
+		library[9].setInfo("The Thousandth Floor", "Katherine McGee", "Science Fiction", true);
+		//book 11
+		library[10].setInfo("Cinder", "Marissa Meyer", "Science Fiction", true);
+		//book 12
+		library[11].setInfo("Scarlet", "Marissa Meyer", "Science Fiction", true);
+		//book 13
+		library[12].setInfo("Cress",  "Marissa Meyer", "Science Fiction", true);
+		//book 14
+		library[13].setInfo("The Golden Compass", "Philip Pullman", "Fantasy", true);
+		//book 15
+		library[14].setInfo("The Lost Hero", "Rick Riordan", "Fantasy", true);
+		//book 16
+		library[15].setInfo("The Son of Neptune", "Rick Riordan", "Fantasy", true);
+		//book 17
+		library[16].setInfo("The Mark of Athena", "Rick Riordan", "Fantasy", true);
+		//book 18
+		library[17].setInfo("The House of Hades", "Rick Riordan", "Fantasy", true);
+		//book 19
+		library[18].setInfo("The Blood of Olympus", "Rick Riordan", "Fantasy", true);
+		//book 20
+		library[19].setInfo("Divergent", "Veronica Roth", "Science Fiction", true);
+		//book 21
+		library[20].setInfo("Insurgent", "Veronica Roth", "Science Fiction", true);
+		//book 22
+		library[21].setInfo("Allegiant", "Veronica Roth", "Science Fiction", true);
+		//book 23
+		library[22].setInfo("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "Fantasy", true);
+		//book 24
+		library[23].setInfo("Harry Potter and the Chamber of Secrets", "J.K. Rowling", "Fantasy", true);
+		//book 25
+		library[24].setInfo("Harry Potter and the Prisoner of Azkaban", "J.K. Rowling", "Fantasy", true);
+		//book 26
+		library[25].setInfo("Harry Potter and the Goblet of Fire", "J.K. Rowling", "Fantasy", true);
+		//book 27
+		library[26].setInfo("Harry Potter and the Order of the Pheonix", "J.K. Rowling", "Fantasy", true);
+		//book 28
+		library[27].setInfo("Harry Potter and the Half-Blood Prince", "J.K. Rowling", "Fantasy", true);
+		//book 29
+		library[28].setInfo("Harry Potter and the Deathly Hallows", "J.K. Rowling", "Fantasy", true);
+		//book 30
+		library[29].setInfo("The Book Theif", "Markus Zusak", "Fiction", true);
+
+		nElems = 30;
+		
 		//create frame
 		frame = new JFrame();
 		frame.getContentPane().setBackground(Color.LIGHT_GRAY); //change color
@@ -108,11 +184,17 @@ public class Library extends LibraryTesterSutter{ //is the extends necessary??
 			public void actionPerformed(ActionEvent e) {
 				if (textField.getText().equals("a")) { //if you want to access a book
 					label.setText("Please enter the book number to access"); //print instructions
-					textField.setText(""); //clear text box
+					textField.setText(null); //clear text box
+					//my problem is the next things are looking at the empty text box.
+					//do i delete the clear text box, or work my way around it
+					//delete clear text box would be quicker
+					//but keeping it is more proper
 					textField.setText(textField.getText()); //set new text
 					bookNumber = Integer.parseInt(textField.getText()); //parse into an integer
-					// PROBLEM LINE
-					label.setText(library[bookNumber]); //print book requested
+					if (bookNumber <= 30) {
+						label.setText("TEST"); //print book requested
+					}
+					
 				}
 				else if (textField.getText().equals("c")) { //if you want to check out a book
 					label.setText("Please enter the book number to check out"); //print instructions
@@ -120,7 +202,7 @@ public class Library extends LibraryTesterSutter{ //is the extends necessary??
 					textField.setText(textField.getText()); //set new text
 					bookNumber = Integer.parseInt(textField.getText()); //parse into an integer
 					//PROBLEM LINE
-					Book.library[bookNumber].setStatus(false);
+					library[bookNumber].setStatus(false);
 					label.setText("Book number: " + bookNumber + "successfully checked out."); //print instructions
 				}
 				else if (textField.getText().equals("r")) { //if you want to return a book
@@ -128,8 +210,7 @@ public class Library extends LibraryTesterSutter{ //is the extends necessary??
 					textField.setText(""); //clear text box
 					textField.setText(textField.getText()); //set new text
 					bookNumber = Integer.parseInt(textField.getText()); //parse into an integer
-					//PROBLEM LINE
-					Book.library[bookNumber].setStatus(true);
+					library[bookNumber].setStatus(true);
 					label.setText("Book number: " + bookNumber + "successfully returned."); //print instructions
 				}
 				else if (textField.getText().equals("v")) { //if you want to check out a book
